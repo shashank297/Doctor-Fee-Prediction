456b716c6a67b106bbf206a330670fea2b72b9d2
Shashank Shukla <Shuklashashank297@gmail.com>
2023-02-20 03:44:52 +0530

    commit message

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+modal=pickle.load(open('pipe.pkl','rb'))
 
 app=Flask(__name__)
 
-@app.route("/",methods=['GET','POST'])
-
+@app.route('/')
 def index():
+    return render_template('index.html')
 
-    return 'Shashank Shukla'
-if __name__=="__main__":
+@app.route('/predict',methods=['post'])
+
+def predict_fee():
+   DP_Score= int(request.form.get('DP Score'))
+   NPV_Value=int(request.form.get('NPV Value'))
+   City=request.form.get('City')
+   Years_of_Experience=int(request.form.get('Years of Experience'))
+   Speciality=request.form.get('Speciality')
+   Degree_no=int(request.form.get('Degree_no'))
+   Degree_1=request.form.get('Degree_1')
+
+   result=modal.predict(np.array([DP_Score,NPV_Value,City,Years_of_Experience,Speciality,Degree_no,Degree_1]).reshape(1,7))
+
+   return str(result[0])
+
+if __name__=='__main__':
     app.run(debug=True)
